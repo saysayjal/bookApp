46f650af5a1b7b115ab49f9a3e71c3b5cdc67341
Sayjal <ssayjal11@gmail.com>
2023-11-03 13:31:11 +0545

    few updates

--- routes/books.js
+++ routes/books.js
@@ -11,4 +11,17 @@ router.post('/save', function(req, res, next){
     res.redirect('/')
 })
 
+router.get('/edit/:_id', function(req, res, next){
+    const book = books.find((book)=>book._id === req.params._id)
+    res.render('editBooks', {title: "Edit Books", book})
+})
+
+
+router.post('/saveEdited/:_id', function(req, res, next){
+    const currIndex = books.findIndex((book)=>book._id === req.params._id)
+    books.splice(currIndex, 1, {...req.body, _id: req.params._id})
+    res.redirect('/')
+  })
+  
+
 module.exports = router;
